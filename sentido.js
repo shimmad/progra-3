28c92e96e8062ef73e5f65266878a8e15d4bc1ab
jime <jimemartinezarana@hotmail.com>
2025-04-28 17:12:22 -0300

    mucha practica!

---
+++ sentido.js
@@ -0,0 +1,16 @@
+// scope de variables(alcance)
+// let= variable dentro de un bloque let numero;
+// numero = 30 o en la misma lineA let numero= 19;
+// let numero1, numero2,numero3;
+// var= alcance global (se pueden modificar en todo lugar, no recomendable)
+//const= no se puede cambiar el valor una vez definidAs. const pi= 3.14;
+// alert() funcion q muestra mensaje en pántalla
+// prompt() pide al usuario un ingreso let nombre=prompt("escribi tu nombrw")
+// document.write("hola" + variable)      
+// dineroclofa= parseInt(dinerocofla); conversion a int  
+// <br> salto de linea
+// for (x in array){} muestra el numero de posicion de cada elemento
+// recorre nombres de propiedades de objetos
+// 1 2 3 
+//for (x of array){} muestra el contenido de cada posicion
+// recorre valores de iterables
